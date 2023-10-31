64aa5b463ce7e4be1aed60aff433f5318cff5a37
Uzair Shah <74507816+Uzshah@users.noreply.github.com>
2023-10-31 16:24:50 +0300

    Update main.py
    Signed-off-by: Uzair Shah <74507816+Uzshah@users.noreply.github.com>

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def main(args):
         train_losses.append(train_loss)
         train_acc.append(train_accuracy)
         print()
-        text = f'Epoch [{epoch}/{args.epochs}], training loss: {train_loss:.4f}, training accuracy: {train_accuracy:.2f}, Total time: {epoch_time:.2f}'
+        text = f'Epoch [{epoch+1}/{args.epochs}], training loss: {train_loss:.4f}, training accuracy: {train_accuracy:.2f}, Total time: {epoch_time:.2f}'
         print(text)
         file.write(text)
         file.write('\n')
