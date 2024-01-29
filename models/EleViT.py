3872dfb948c4726b98293a3b0cc27100cc2e9c46
Uzair Shah <74507816+Uzshah@users.noreply.github.com>
2024-01-29 11:24:35 +0300

    Update EleViT.py
    Signed-off-by: Uzair Shah <74507816+Uzshah@users.noreply.github.com>

--- models/EleViT.py
+++ models/EleViT.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import math
 from timm.models.registry import register_model
 from einops import rearrange
+from timm.models.layers import DropPath, trunc_normal_
+
+
 
 def stem(in_chs, out_chs):
     """
@@ -13,101 +16,64 @@ def stem(in_chs, out_chs):
     Output: sequence of layers with final shape of [B, C, H/4, W/4]
     """
     return nn.Sequential(
-        nn.Conv2d(in_chs, out_chs //2, kernel_size=3, stride=2, padding=1),
+        nn.Conv2d(in_chs, out_chs//2, kernel_size=3, stride=1, padding=1),
         nn.BatchNorm2d(out_chs//2),
         nn.ReLU(),
-        nn.Conv2d(out_chs // 2, out_chs, kernel_size=3, stride=2, padding=1),
+        nn.MaxPool2d(3, 2, padding = 1),
+        nn.Conv2d(out_chs//2, out_chs, kernel_size=3, stride=1, padding=1),
         nn.BatchNorm2d(out_chs),
-        nn.ReLU(), )
-
-class Attention(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation_rate=1, bias=False, num_heads = 4):
-        super().__init__()
-        self.num_heads = num_heads
-        self.head_dim = out_channels // num_heads
-        # Split the input into multiple heads
-        self.query_heads = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride,
-                                                                 padding, bias=bias, dilation=dilation_rate),
-                                                        nn.BatchNorm2d(out_channels))
-        self.key_heads = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride,
-                                                                 padding, bias=bias, dilation=dilation_rate),
-                                                        nn.BatchNorm2d(out_channels))
-        self.value_heads = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride,
-                                                                 padding, bias=bias, dilation=dilation_rate),
-                                                        nn.BatchNorm2d(out_channels))
-
-        # Modify the residual connection
-        self.downsample = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1,
-                          stride=stride, bias=False, dilation=dilation_rate,padding=1),
-                nn.BatchNorm2d(out_channels)
-            )
-
-
-    def forward(self, x):
-        query = self.query_heads(x)
-        key = self.key_heads(x)
-        value = self.value_heads(x)
-
-        # Calculate scaling factor
-
-        scores = torch.einsum('b c w j,b c j k->b c w k', query, key.transpose(-1, -2))
-        attention_weights = nn.functional.softmax(scores, dim=-1)
-
-        head_output = torch.einsum('bchw,bchw->bchw', attention_weights, value)  # Attention output for this head
-
-        # Residual connection and further processing
-        residual = self.downsample(x)
-        out = head_output + residual
-        return out
-   
-
+        nn.ReLU(), 
+        nn.MaxPool2d(3, 2, padding = 1),)
+    
+    
 class MultiheadAttention(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation_rate=1,
-                 num_heads=8,bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size=7, stride=1, padding=3, dilation_rate=1,
+                 num_heads=8,bias=False, drop_out = 0.0):
         super(MultiheadAttention, self).__init__()
         self.num_heads = num_heads
         self.head_dim = out_channels // num_heads
         # Split the input into multiple heads
-        self.query = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride, 
-                                                    padding, bias=bias, dilation=dilation_rate),
-                                                    nn.BatchNorm2d(out_channels))
-        self.key = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride, 
-                                                    padding, bias=bias, dilation=dilation_rate),
-                                                    nn.BatchNorm2d(out_channels))
-        self.value = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride, 
-                                                    padding, bias=bias, dilation=dilation_rate),
-                                                    nn.BatchNorm2d(out_channels))
-
+        self.qkv_conv = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * 3, kernel_size, 
+                          1, padding, bias=bias, dilation=dilation_rate),
+                nn.BatchNorm2d(out_channels * 3)
+            )
+        self.apply(self._init_weights)
         self.scale = math.sqrt(self.head_dim)
         self.soft = nn.Softmax(dim = -1)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.gamma = nn.Parameter(torch.zeros(1))
+        #self.talking_head1 = nn.Conv2d(self.head_dim, self.head_dim, kernel_size=1, stride= 1, padding=0)
+        #self.talking_head2 = nn.Conv2d(self.head_dim, self.head_dim, kernel_size=1, stride=1, padding=0)
+        #self.gamma = nn.Parameter(torch.zeros(1))
+        #self.attention_bias = nn.Parameter(torch.zeros(self.head_dim))
         self.focusing_factor = nn.Parameter(torch.zeros(1))
-        self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, 
-                          stride=stride, bias=False, dilation=dilation_rate),
-                nn.BatchNorm2d(out_channels)
-            ) if (stride != 1 or in_channels != out_channels) else nn.Identity()
-            
-
+        self.act = nn.GELU()
+        self.drop = nn.Dropout(drop_out) if drop_out> 0 else nn.Identity()
+        
+        
+  
+    def _init_weights(self, m):
+        if isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+                
+                
     def forward(self, x):
-        query = self.query(x)
-        key = self.key(x)
-        value = self.value(x)
-        q = rearrange(query, 'b (c h) w j -> b c h w j', h = self.num_heads)
-        k = rearrange(key, 'b (c h) w j -> b c h w j', h = self.num_heads)
-        v = rearrange(value, 'b (c h) w j -> b c h w j', h = self.num_heads)
-        scores = torch.einsum('b c h w j,b c h j k->b c h w k', q, k.transpose(-1, -2))/self.scale
+        qkv = self.qkv_conv(x)
+        q, k, v = torch.chunk(qkv, 3, dim=1)
+        q, k, v = map(lambda t: rearrange(t, 'b (c h) w j -> (b h) c w j', h = self.num_heads),(q, k, v))
+        scores = torch.einsum('b c w j,b c j k->b c w k', q, k.transpose(-1, -2))/self.scale
+        # Add the parameterized attention bias term here
+        #scores += self.attention_bias.view(1, self.head_dim, 1, 1)
+        #scores = self.talking_head1(scores)
         attention_weights = self.soft(scores)
-        multihead_output = torch.einsum('b c h w j,b c h w j->b c h w j', attention_weights, v*self.focusing_factor)
-        multihead_output = rearrange(multihead_output, 'b c h w j -> b (c h) w j')
-        residual = self.downsample(x)
-        out = self.gamma * multihead_output + residual
-        out = self.bn(out)
-        return out
+        #attention_weights = self.talking_head2(attention_weights)
+        multihead_output = torch.einsum('b c w j,b c w j->b c w j', attention_weights*self.focusing_factor, v)
+        out = rearrange(multihead_output, '(b h) c w j -> b (c h) w j', h = self.num_heads)
+        out = self.drop(out)
+        out = self.act(out)
+        return self.bn(out)
 
 
 class ConvEncoder(nn.Module):
@@ -124,28 +90,14 @@ class ConvEncoder(nn.Module):
         self.pwconv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         self.act = nn.GELU()
         self.pwconv2 = nn.Conv2d(out_channels, out_channels, kernel_size=1)
-        # Modify the residual connection
-        self.downsample = nn.Sequential()
-        if stride != 1 or in_channels != out_channels:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3,
-                          stride=stride, bias=False, padding=1),
-                nn.BatchNorm2d(out_channels)
-            )
         
 
     def forward(self, x):
-        input = x
-        #print(x.size())
         x = self.dwconv(x)
         x = self.norm(x)
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
-        #print(x.size())
-        res = self.downsample(input)
-        #print(res.size())
-        x = res + x
         return x
         
 
@@ -157,15 +109,17 @@ class Embedding(nn.Module):
     Output: tensor in shape [B, C, H/stride, W/stride]
     """
 
-    def __init__(self, patch_size=3, stride=2, padding=0,
+    def __init__(self, patch_size=3, stride=1, padding=0,
                  in_chans=3, embed_dim=768, norm_layer=nn.BatchNorm2d):
         super().__init__()
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size,
                               stride=stride, padding=padding)
+        self.maxpool = nn.MaxPool2d(3, 2, padding = 1)
         self.norm = norm_layer(embed_dim) if norm_layer else nn.Identity()
 
     def forward(self, x):
         x = self.proj(x)
+        x = self.maxpool(x)
         x = self.norm(x)
         return x
 
@@ -196,38 +150,51 @@ class Mlp(nn.Module):
         x = self.fc2(x)
         x = self.drop(x)
         return x
+
+class AttnFFN(nn.Module):
+    def __init__(self, dim, drop=0., drop_path=0.):
+        super().__init__()
+        self.conv = ConvEncoder(dim, dim)
+        self.attn = MultiheadAttention(dim, dim, kernel_size = 3, padding=1, stride= 1, num_heads = 4)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. \
+            else nn.Identity()
+        
+    def forward(self, x):
+        x = x + self.conv(x)
+        x = x + self.attn(x)
+        return x    
+    
     
 class EleViT(nn.Module):
-    def __init__(self, num_classes=100, d_model=256, depth = 12):
+    def __init__(self, num_classes=100, d_model=256, depth = 12, drop_path=0.):
         super(EleViT, self).__init__()
         
-        self.stem = stem(3, 128)
+        self.drop_path = drop_path
+        self.stem = stem(3, 40)
         
-        self.attn1 = nn.ModuleList([nn.Sequential(ConvEncoder(128, 128),
-                                                  MultiheadAttention(128, 128)) for _ in range(2)])
-        self.embed1 = Embedding(in_chans=128, embed_dim = 256)
+        self.attn1 =self._make_layer(40, 2)
+        self.embed1 = Embedding(in_chans=40, embed_dim = 80)
+        self.attn2 = self._make_layer(80, 2)
+        self.embed2 = Embedding(in_chans=80, embed_dim = 256)
+        self.attn3 = self._make_layer(256, 4)
+        self.embed3 = Embedding(in_chans=256, embed_dim = 512)
+        self.attn4 = self._make_layer(512, 2)
         
-        self.attn2 = nn.ModuleList([nn.Sequential(ConvEncoder(d_model, d_model),
-                                                  MultiheadAttention(256, 256)) for _ in range(3)])
-        self.embed2 = Embedding(in_chans=256, embed_dim = 288)
-        
-        self.attn3 = nn.ModuleList([nn.Sequential(ConvEncoder(288, 288),
-                                                  MultiheadAttention(288,288)) for _ in range(3)])
-        self.embed3 = Embedding(in_chans=288, embed_dim = 384)
-        
-        self.attn4 = nn.ModuleList([nn.Sequential(ConvEncoder(384,384),
-                                                  MultiheadAttention(384,384)) for _ in range(2)])
-        
-        
+        ## classification head
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dist_head = nn.Linear(
-                    384, num_classes) if num_classes > 0 \
+                    512, num_classes) if num_classes > 0 \
                     else nn.Identity()
-  
         self.head = nn.Linear(
-                384, num_classes) if num_classes > 0 \
+                512, num_classes) if num_classes > 0 \
                 else nn.Identity()
-        
+    
+    def _make_layer(self, dim, num):
+        layers = []
+        for _ in range(num):
+            layers.append(AttnFFN(dim, drop_path = self.drop_path))
+        return nn.Sequential(*layers)
+    
     def forward(self, x):
         x = self.stem(x)   
         for layer in self.attn1:
